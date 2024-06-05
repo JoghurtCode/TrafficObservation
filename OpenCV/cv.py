0f96bb84ff53f17d57780c6c17242909e95547b6
JoghurtCode <jann.theis@edu.fhdw.de>
2024-06-05 10:52:36 +0200

    Improved Detetion algorithm

--- OpenCV/cv.py
+++ OpenCV/cv.py
@@ -3,17 +3,18 @@ import numpy as np
 
 def main():
     cap = cv2.VideoCapture('cars.mp4')
-    
     backSub = cv2.createBackgroundSubtractorMOG2(detectShadows=True)
 
     car_count = 0
-    min_contour_width = 40  
-    min_contour_height = 40 
-    min_contour_area = 500  
-    max_contour_area = 5000 
+    min_contour_width = 40
+    min_contour_height = 40
+    min_contour_area = 500
+    max_contour_area = 50000
 
     counting_line_position = 550
     offset = 10
+
+    kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (5, 5))
 
     while True:
         ret, frame = cap.read()
@@ -21,29 +22,23 @@ def main():
             break
 
         fg_mask = backSub.apply(frame)
-
         _, fg_mask = cv2.threshold(fg_mask, 200, 255, cv2.THRESH_BINARY)
-
-        fg_mask = cv2.morphologyEx(fg_mask, cv2.MORPH_OPEN, None, iterations=2)
-        fg_mask = cv2.morphologyEx(fg_mask, cv2.MORPH_CLOSE, None, iterations=2)
+        fg_mask = cv2.morphologyEx(fg_mask, cv2.MORPH_OPEN, kernel, iterations=3)
+        fg_mask = cv2.morphologyEx(fg_mask, cv2.MORPH_CLOSE, kernel, iterations=3)
 
         contours, _ = cv2.findContours(fg_mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
         for contour in contours:
-            x, y, w, h = cv2.boundingRect(contour)
-
-            if w >= min_contour_width and h >= min_contour_height and min_contour_area < cv2.contourArea(contour) < max_contour_area:
+            if min_contour_area < cv2.contourArea(contour) < max_contour_area:
+                x, y, w, h = cv2.boundingRect(contour)
                 aspect_ratio = w / float(h)
-                if 0.2 < aspect_ratio < 4.0:
+                if w >= min_contour_width and h >= min_contour_height and 0.2 < aspect_ratio < 4.0:
                     cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
-
                     if (y + h) > (counting_line_position - offset) and (y + h) < (counting_line_position + offset):
                         car_count += 1
 
         cv2.line(frame, (0, counting_line_position), (frame.shape[1], counting_line_position), (255, 0, 0), 2)
-
         cv2.putText(frame, f'Car Count: {car_count}', (10, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 255), 2, cv2.LINE_AA)
-
         cv2.imshow('Frame', frame)
 
         if cv2.waitKey(30) & 0xFF == ord('q'):
